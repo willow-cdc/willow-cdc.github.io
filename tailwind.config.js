39be19907648727c31635941c1198d5ebcc935ec
Alex Bair <alexbair143@gmail.com>
2024-04-18 00:02:55 -0400

    chore: :wrench: Add custom colors and Roboto font to tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,42 @@ module.exports = {
     preflight: false, // disable Tailwind's reset
   },
   content: ["./src/**/*.{js,jsx,ts,tsx}", "../docs/**/*.mdx"], // my markdown stuff is in ../docs, not /src
-  darkMode: ["class", '[data-theme="dark"]'], // hooks into docusaurus' dark mode settigns
+  darkMode: ["class", '[data-theme="dark"]'], // hooks into docusaurus' dark mode settings
   theme: {
     extend: {
       fontFamily: {
         epilogue: ["Epilogue", "sans-serif"],
         sans: ["Inter", "sans-serif"],
+        roboto: ['Roboto', 'sans-serif'],
       },
       colors: {
-        primary: "#409935",
-        primaryLight: "#56cd47",
+        willowGreen: {
+          '50': '#f3fbf2',
+          '100': '#e5f6e2',
+          '200': '#cbecc6',
+          '300': '#a1dc99',
+          '400': '#70c365',
+          '500': '#4ba740',
+          '600': '#409935',
+          '700': '#2f6d28',
+          '800': '#295724',
+          '900': '#23481f',
+          '950': '#0e270c',
+        },
+        willowGreenLight: "#56cd47",
+        willowBrown: {
+          '50': '#f5f4f0',
+          '100': '#e9e8de',
+          '200': '#d6d4c0',
+          '300': '#bbb89b',
+          '400': '#a29f79',
+          '500': '#7c7955',
+          '600': '#696747',
+          '700': '#525139',
+          '800': '#434231',
+          '900': '#3b3b2c',
+          '950': '#1e1e15',
+        },
       },
     },
   },
